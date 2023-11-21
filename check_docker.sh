70e5f5563165f3797bc2d69bd67288dffc51d31a
Leander Lingens <64893800+leanderlingens@users.noreply.github.com>
2023-11-21 13:44:53 +0100

    script exchanged

--- check_docker.sh
+++ check_docker.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
 
-# Source: https://github.com/jryberg/nagios-plugin
+# Source: https://github.com/jryberg/nagios-plugin/blob/master/check_docker.sh
 
+#
 # -------------------------------------- Description ---------------------------------------------------
 #
 # Monitoring Docker CPU / MEM / Network / Status
 # Usage example :
-# ./check_docker.sh -n <container name> -c <PERC_WARNING_CPU>,<PERC_CRITCAL_CPU>
-# ./check_docker.sh -n <container name> -m <PERC_WARNING_MEM>,<PERC_CRITICAL_MEM>
+# ./check_docker.sh -n <container name> -c <PERC_WARNING_CPU>,<PERC_CRITCAL_CPU> \
+# -m <PERC_WARNING_MEM>,<PERC_CRITICAL_MEM> \
+# -N <WARNING_NET_RX,WARNING_NET_TX,CRITCAL_NET_RX,CRITCAL_NET_TX>
 #
 # ------------------------------------- Requierements --------------------------------------------------
 #
@@ -18,97 +20,159 @@
 # ---------------------------------------- License -----------------------------------------------------
 # 
 # This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
 #
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
+#  This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
 #
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>. 
+#   You should have received a copy of the GNU General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>. 
 #
-# contribute to https://github.com/jryberg
 #----------------------------------------------------------------------------------------------------------
 
-VERSION="Version 0.1"
-AUTHOR="2019 by janlueders"
+VERSION="Version 1.1"
+AUTHOR="2017 by sysC0D"
 STATE_OK=0
 STATE_WARN=1
 STATE_CRIT=2
+STATE_UNKN=3
 
-## PRINT VERSION #
-function version {
-	echo "$VERSION"
-}
-
-
-## Container exists ##
-function dockerexist () {
-        dckname="$1"
-        container=`sudo docker stats --no-stream | grep $dckname`
-
-        if [ ! -z "$container" ]
-        then
-                echo "true"
-	fi
-}
-
+#
+## HELP ##
+#
 
 function print_help {
-        echo "Usage: ./check_docker.sh [-v] [-h] [-n [-s -c -m -ir -iw -nu -nd]]
+        echo "Usage: ./check_docker_stats.sh [-v] [-h] [-n [-s -c -m -N]]
         -h, --help
         print this help message
         -v, --version
         print version program
         -n, --name
         name docker to check
-        -c, --cpulimit PERC_WARNING_CPU,PERC_CRITCAL_CPU
+        -c, --cpulimitperc PERC_WARNING_CPU,PERC_CRITCAL_CPU
         number in percent alert for CPU
-        -m, --memlimit PERC_WARNING_MEM,PERC_CRITICAL_MEM
+        -m, --memlimitperc PERC_WARNING_MEM,PERC_CRITICAL_MEM
         number in percent alert for MEM usage
-        -ir, --ioread MB_WARNING_IO_READ,MB_CRITICAL_IO_READ
-        the I/O Read with waring and critial in MB
-        -iw, --iowrite MB_WARNING_IO_WRITE,MB_CRITICAL_IO_WRITE
-        the I/O Write with warning and critical in MB 
+        -N, --ntwlimit WARNING_NET_RX,WARNING_NET_TX,CRITCAL_NET_RX,CRITCAL_NET_TX
+        bandwidth (RX -> DL,TX -> UL) in kBps for docker selected
         -s, --status
-        check if docker is alive
-        -nu, --netupload GB_WARNING_UPLOAD,GB_CRITICAL_UPLOAD
-        number in gb alert for upload
-        -nd, --netdownload GB_WARNING_DOWNLOAD,GB_CRITICAL_DOWNLOAD"
+        check if docker is alive"
+        ##SOON
+        #-M, --memlimit=WARNING_MEM,CRITCAL_MEM
+        #number in Byte for MEM Usage
 }
 
+#
+## PRINT VERSION #
+#
+
+function print_version {
+        echo "$VERSION $AUTHOR"
+}
+
+#
+## Exist Docker ##
+#
+
+function ifexist () {
+        local namedocker="$1"
+        checkdocker=`sudo docker ps --filter "name=^/$namedocker\$" | grep $namedocker`
+
+        if [ ! -z "$checkdocker" ]
+        then
+                echo "true"
+        fi
+}
+
+#
+## GET SHORT ID ## 
+#
+
+function getshortiddocker () {
+        local namedocker=$1
+
+        shortid=`sudo docker ps --filter "name=^/$namedocker\$" | grep $namedocker | awk '{print $1}'`
+        echo "$shortid"
+}
+
+#
+## GET BANDWIDTH ##
+#
+
+function getrxtxdocker () {
+        local instanceid=$1
+
+        #Get Container ID
+        CID=`sudo docker inspect --format='{{.Id}}' $instanceid`
+
+        #Get RX-TX Virtual interface
+        TASKS=/sys/fs/cgroup/devices/docker/$CID*/tasks
+        PID=$(head -n 1 $TASKS)
+        mkdir -p /var/run/netns
+        ln -sf /proc/$PID/ns/net /var/run/netns/$CID
+        VETH=`ip netns exec $CID netstat -i | grep "eth0" | awk '{print $4":"$8}'`
+
+        echo "$VETH"
+}
+
+function getbandwidthdocker () {
+        local instanceid=$1
+
+        #Get RXTX
+        rxtx1=$(getrxtxdocker $instanceid)
+        sleep 1
+        rxtx2=$(getrxtxdocker $instanceid)
+
+        #GET RBPS-TBPS
+        R1=`echo "$rxtx1" | awk '{split($0,a,":"); print a[1]}'`
+        T1=`echo "$rxtx1" | awk '{split($0,a,":"); print a[2]}'`
+        R2=`echo "$rxtx2" | awk '{split($0,a,":"); print a[1]}'`
+        T2=`echo "$rxtx2" | awk '{split($0,a,":"); print a[2]}'`
+        RBPS=`expr $R2 - $R1`
+        TBPS=`expr $T2 - $T1`
+
+        #GET  RBPS-TBPS
+        RKBPS=`expr $RBPS / 1024`
+        TKBPS=`expr $TBPS / 1024`
+        echo "${RKBPS},${TKBPS}"
+}
+
+#
+## STATS Docker ## 
+#
+
 function getstats () {
+        #CPUPerc - MemUsage - MemPerc - NetIO - BlockIO
         local typestats="$1"
-	local cid="$2"
-		case "$typestats" in
-  		      CPUUsage)
-			currentstats=`sudo docker stats --no-stream | grep $cid | awk '{print $3}'`
-			;;
-                      IORead)
-			currentstats=`sudo docker stats --no-stream | grep $cid | awk '{print $11}'`
-			;;
-                      IOWrite)
-                        currentstats=`sudo docker stats --no-stream | grep $cid | awk '{print $13}'`
+        local cid="$2"
+        if [[ "$valid_version" == "1" ]]
+        then
+                currentstats=`sudo docker stats --no-stream --format "{{.Container}}|{{.$typestats}}" | grep $cid`
+                echo "$currentstats" | awk '{split($0,a,"|"); print a[2]}'
+        else
+                case "$typestats" in
+                      CPUPerc)
+                        currentstats=`sudo docker stats --no-stream | grep $cid | awk '{print $2}'`
                         ;;
-                      Upload)
-                        currentstats=`sudo docker stats --no-stream | grep $cid | awk '{print $10}'`
-                        ;;
-                      Download)
+                      MemPerc)
                         currentstats=`sudo docker stats --no-stream | grep $cid | awk '{print $8}'`
                         ;;
-        	      MemUsage)
-			currentstats=`sudo docker stats --no-stream | grep $cid | awk '{print $7}'`
-			;;
-		      *)
-            		echo "Unknown argument: $typestats"
-            		exit $STATE_UNKN
-            		;;
-     		esac
-		echo "$currentstats"
+                      *)
+                        echo "Unknown argument: $typestats"
+                        exit $STATE_UNKN
+                        ;;
+                esac
+                echo "${currentstats::-1}"
+        fi
 }
+
+#
+## Function monitoring perc ##
+#
 
 function monitorperc () {
         local typestats="$1"
@@ -121,168 +185,203 @@ function monitorperc () {
                 valcurrentp=$(getstats $typestats $shortid)
                 vallimitwarn=`echo "$arg_limit" | awk '{split($0,a,","); print a[1]}'`
                 vallimitcrit=`echo "$arg_limit" | awk '{split($0,a,","); print a[2]}'`
-		valcurrent=${valcurrentp::-1}
-		valcur=`echo "${valcurrentp//i}"`
-		lowercase_current_val=`echo "$typestats" | awk '{print tolower($0)}'`
-		valcur=`echo "$valcur" | awk '{print tolower($0)}'`
-
-		if (( $(echo "$valcurrent > $vallimitcrit" |bc -l) ))
+                valcurrent=${valcurrentp::-1}
+                if (( $(echo "$valcurrent > $vallimitcrit" |bc -l) ))
                 then
-                        echo ", CRITICAL - $typestats: $valcurrentp|$lowercase_current_val=$valcur;$vallimitwarn;$vallimitcrit;;"
+                        echo ", CRITICAL $typestats: $valcurrent"
                 elif (( $(echo "$valcurrent > $vallimitwarn" |bc -l) ))
                 then
-                        echo "WARNING - $typestats: $valcurrentp|$lowercase_current_val=$valcur;$vallimitwarn;$vallimitcrit;;"
-		else
-			echo "OK - $typestats $valcurrentp|$lowercase_current_val=$valcur;$vallimitwarn;$vallimitcrit;;"
-		fi 
+                        echo ", WARNING $typestats: $valcurrent"
+                else
+                        echo ", $typestats $valcurrentp"
+                fi 
        else
                 echo "Value $arg_limit malformed, please show --help"
        fi
 }
 
-arg_containername=""
+#
+## Function monitoring Network ##
+#
+
+function monitorbandwith () {
+        #WARNING_NET_RX,WARNING_NET_TX,CRITCAL_NET_RX,CRITCAL_NET_TX
+        local arg_limit="$1"
+        local shortid="$2"
+        formatregbd='^[0-9]+,[0-9]+,[0-9]+,[0-9]+$'
+
+        if  [[ $arg_limit =~ $formatregbd ]]
+        then
+                valcurrentrxtx=$(getbandwidthdocker $shortid)
+                valcurrentrx=`echo "$valcurrentrxtx" | awk '{split($0,a,","); print a[1]}'`
+                valcurrenttx=`echo "$valcurrentrxtx" | awk '{split($0,a,","); print a[1]}'`
+
+                vallimitwarnrx=`echo "$arg_limit" | awk '{split($0,a,","); print a[1]}'`
+                vallimitwarntx=`echo "$arg_limit" | awk '{split($0,a,","); print a[2]}'`
+                vallimitcritrx=`echo "$arg_limit" | awk '{split($0,a,","); print a[3]}'`
+                vallimitcrittx=`echo "$arg_limit" | awk '{split($0,a,","); print a[4]}'`
+
+                if [ "$valcurrentrx" -gt "$vallimitcritrx" ]
+                then
+                        echo ", CRITICAL Network DL : ${valcurrentrx}kbps"
+                elif [ "$valcurrentrx" -gt "$vallimitwarnrx" ]
+                then
+                        echo ", WARNING Network DL : ${valcurrentrx}kbps"
+                else
+                        echo ", DL ${valcurrentrx}kbps"
+                fi
+
+                if [ "$valcurrenttx" -gt "$vallimitcrittx" ]
+                then
+                        echo ", CRITICAL $valcurrenttx"
+                elif [ "$valcurrenttx" -gt "$vallimitwarntx" ]
+                then
+                        echo ", WARNING $valcurrenttx"
+                else
+                        echo ", UL ${valcurrentrx}kbps"
+                fi
+        else
+                echo "Value $arg_limit malformed, please show --help"
+        fi
+}
+
+#
+## MAIN
+#
+
+arg_namedocker=""
 arg_cpulimitperc=""
 arg_memlimitperc=""
-arg_ioread=""
-arg_iowrite=""
-arg_netupload=""
-arg_netdownload=""
+arg_memlimit=""
+arg_ntwlimit=""
 arg_checkstatus=""
 
 while test -n "$1"; do
     case "$1" in
-	-h|--help)
-	    print_help
-	    exit "$STATE_OK"
+        -h|--help)
+            print_help
+            exit "$STATE_OK"
             ;;
-	-v|--version)
+        -v|--version)
             print_version
-	    exit "$STATE_OK"
-	    ;;
-	-n|--name)
-	    arg_containername=$2
-	    shift
-	    ;;
-	-c|--cpulimitperc)
-	    arg_cpulimitperc=$2
-	    shift
-	    ;;
-	-m|--memlimitperc)
-	    arg_memlimitperc=$2
-	    shift
-	    ;;
-	-ir|--ioread)
-	    arg_ioread=$2
-	    shift
-	    ;;
-	-iw|--iowrite)
-	    arg_iowrite=$2
-	    shift
-	    ;;
-	-nu|--netupload)
-	    arg_netupload=$2
-	    shift
-	    ;;
-        -nd|--netdownload)
-            arg_netdownload=$2
+            exit "$STATE_OK"
+            ;;
+        -n|--name)
+            arg_namedocker=$2
             shift
             ;;
-	-s|--status)
-	    arg_checkstatus="true"
-	    ;;
-	*)
-	    echo "Unknown argument: $1"
-	    print_help
-	    exit $STATE_CRIT
-	    ;;
+        -c|--cpulimitperc)
+            arg_cpulimitperc=$2
+            shift
+            ;;
+        -m|--memlimitperc)
+            arg_memlimitperc=$2
+            shift
+            ;;
+        -M|--memlimit)
+            arg_memlimit=$2
+            shift
+            ;;
+        -N|--ntwlimit)
+            arg_ntwlimit=$2
+            shift
+            ;;
+        -s|--status)
+            arg_checkstatus="true"
+            ;;
+        *)
+            echo "Unknown argument: $1"
+            print_help
+            exit $STATE_UNKN
+            ;;
      esac
      shift
 done
 
+#Check version docker
 valid_version=0
 version_docker=`docker --version | awk '{split($0,a,","); print a[1]}' | sed "s/Docker version //g"`
 major_param=`echo "$version_docker" | awk '{split($0,a,"."); print a[1]}'`
 minor_param=`echo "$version_docker" | awk '{split($0,a,"."); print a[2]}'`
 if [ "$major_param" -ge "1" ] && [ "$minor_param" -ge "13" ]
 then
-	valid_version=1
+        valid_version=1
 elif [ "$major_param" -ge "1" ] && [ "$minor_param" -ge "10" ]
 then
-	valid_version=0
+        valid_version=0
 elif [ "$major_param" -ge "1" ]
 then
         valid_version=1
 else
-	echo "Docker version must be higher 1.10.0 for use this plugin"
-	exit $STATE_CRIT
+        echo "Docker version must be higher 1.10.0 for use this plugin"
+        exit $STATE_UNKN
 fi
 
-if [ ! -z $arg_containername ]
+if [ ! -z $arg_namedocker ]
 then
-	is_docker=$(dockerexist $arg_containername)
+        existdocker=$(ifexist $arg_namedocker)
+        if [[ $existdocker == "true" ]]
+        then 
+                shortid=$(getshortiddocker $arg_namedocker)
 
-	if [[ $is_docker == "true" ]]
-	then
+                statscpu="CPUPerc"
+                statsmemu="MemUsage"
+                statsmem="MemPerc"
 
-		statscpu="CPUUsage"
-		statsmemu="MemUsage"
-		statsioread="Read"
-		statsiowrite="Write"
-		statsupload="Upload"
-		statdownload="Download"
+                valstatus=""
+                valcpup=""
+                valmemp=""
+                valnet=""
 
-		if [[ $arg_checkstatus == "true" ]]
+                if [[ $arg_checkstatus == "true" ]]
                 then
-			metricval=" running|running=1;0;0;;"
+                        statusdocker=`sudo docker ps --filter status=running | grep $shortid`
+                        if [[ $statusdocker == *"$arg_namedocker" ]]
+                        then
+                                valstatus=", running"    
+                        else
+                                echo "$arg_namedocker not running"
+                                exit $STATE_CRIT
+                        fi
                 fi
 
-		if [ ! -z $arg_cpulimitperc ] 
-		then
-			metricval=`echo "$(monitorperc $statscpu $arg_cpulimitperc $arg_containername)"`
-		fi
-
-		if [ ! -z $arg_memlimitperc ]
+                if [ ! -z $arg_cpulimitperc ] 
                 then
-                	metricval=`echo "$(monitorperc $statsmemu $arg_memlimitperc $arg_containername)"`
-		fi
-
-		if [ ! -z $arg_ioread ]
-                then
-                	metricval=`echo "$(monitorperc $statsioread $arg_ioread $arg_containername)"`
-		fi
-
-                if [ ! -z $arg_iowrite ]
-                then
-                        metricval=`echo "$(monitorperc $statsiowrite $arg_iowrite $arg_containername)"`
+                        valcpup=`echo "$(monitorperc $statscpu $arg_cpulimitperc $shortid)"`
                 fi
 
-                if [ ! -z $arg_netupload ]
+                if [ ! -z $arg_memlimitperc ]
                 then
-                        metricval=`echo "$(monitorperc $statsupload $arg_netupload $arg_containername)"`
+                        valmemp=`echo "$(monitorperc $statsmem $arg_memlimitperc $shortid)"`
                 fi
 
-                if [ ! -z $arg_netdownload ]
+                if [ ! -z $arg_ntwlimit ]
                 then
-                        metricval=`echo "$(monitorperc $statsdownload $arg_netdownload $arg_containername)"`
+                        valnet=`echo "$(monitorbandwith $arg_ntwlimit $shortid)"`
                 fi
-	else
-		echo "Docker not running or not exist"
-		exit $STATE_CRIT
-	fi
+        else
+                echo "Docker $arg_namedocker not running or not exist"
+                exit $STATE_UNKN
+        fi
 
-        if [[ $metricval == *"CRITICAL"* ]]
-	then 
-        	echo "${metricval}"
+        valcheck=${valcpup}${valmemp}${valnet}
+        if [[ $valcheck == *"CRITICAL"* ]]
+        then 
+                echo "${arg_namedocker}${valcheck}"
                 exit $STATE_CRIT
-        elif [[ $metricval == *"WARNING"* ]]
-	then
-		echo "${metricval}"
-		exit $STATE_WARN
-	else
-		echo "${metricval}"
-		exit $STATE_OK
-	fi
+        elif [[ $valcheck == *"WARNING"* ]]
+        then
+                echo "${arg_namedocker}${valcheck}"
+                exit $STATE_WARN
+        elif [[ $valcheck == *"--help"* ]]
+        then
+                echo "$arg_namedocke UNKNOW - Please show --help"
+                exit $STATE_UNKN
+        else
+                echo "$arg_namedocker OK ${valstatus}${valcheck}"
+                exit $STATE_OK
+        fi
 else
-	echo "Docker Name required"
-	exit $STATE_CRIT
+        echo "Docker Name required, please show --help"
+        exit $STATE_UNKN
 fi
